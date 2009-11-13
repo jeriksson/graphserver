519ceeba19807c5c9007326bde1151e4b5598b0f
James Biagioni <james@transitgenie.com>
2009-11-13 16:15:58 -0600

    added postgres osmdb and gtfsdb handlers to project

--- pygs/graphserver/ext/gtfs/pggtfsdb.py
+++ pygs/graphserver/ext/gtfs/pggtfsdb.py
@@ -2,7 +2,7 @@
 # Class for handling interaction with GTFSDB stored in PostgreSQL with PostGIS extensions.
 # Author: James P. Biagioni (jbiagi1@uic.edu)
 # Company: University of Illinois at Chicago
-# Last modified: 6/3/09
+# Last modified: 11/2/09
 #
 
 import psycopg2
@@ -14,16 +14,51 @@ class PostgresGIS_GTFSDB:
     #
     def __init__(self, db_connect_string):
         
+        # store the connection string
+        self.db_connect_string = db_connect_string
+        
         # connect to database
-        self.conn = psycopg2.connect(db_connect_string)
+        #self.conn = psycopg2.connect(db_connect_string)
     
     #
-    # method for returning the points along a transit path
+    # method for running a remote query against the PostgreSQL database
     #
-    def get_transit_path_points(self, trip_id, board_stop_id, alight_stop_id):
+    def execute(self, query, args=None):
+        
+        # connect to database
+        conn = psycopg2.connect(self.db_connect_string)
         
         # grab database cursor
-        cur = self.conn.cursor()
+        cur = conn.cursor()
+        
+        # execute remote query with or without arguments
+        if (args is None):
+            cur.execute(query)
+        else:
+            cur.execute(query, args)
+        
+        # send commit to the database
+        conn.commit()
+        
+        # store the query result
+        query_result = cur.fetchall()
+        
+        # close database connection
+        conn.close()
+        
+        # return the query result
+        return query_result
+    
+    #
+    # method for returning all the points along a transit path
+    #
+    def get_all_transit_path_points(self, trip_id):
+        
+        # connect to database
+        conn = psycopg2.connect(self.db_connect_string)
+        
+        # grab database cursor
+        cur = conn.cursor()
         
         # execute query to get trip shape id
         cur.execute("select shape_id from trips where trip_id='" + str(trip_id) + "'")
@@ -32,7 +67,71 @@ class PostgresGIS_GTFSDB:
         shape_id = cur.fetchone()[0]
         
         # send commit to the database
-        self.conn.commit()
+        conn.commit()
+        
+        # execute query to get the list of all points along the shape
+        cur.execute("select ST_AsText(location) from shapes where shape_id='" + shape_id + "' order by shape_pt_sequence asc")
+        
+        # grab list of points along the the shape
+        path_points = cur.fetchall()
+        
+        # send commit to the database
+        conn.commit()
+        
+        # iterate through points
+        for i in range(len(path_points)):
+            mod_point = path_points[i][0].replace('POINT(','').replace(')','').replace(' ',',')
+            point_lat = mod_point[mod_point.index(',')+1:]
+            point_lon = mod_point[0:mod_point.index(',')]
+            path_points[i] = point_lat + ',' + point_lon
+        
+        # close database connection
+        conn.close()
+        
+        # return transit path points
+        return path_points
+    
+    #
+    # method for returning the points along a transit path between board_stop_id and alight_stop_id
+    #
+    def get_transit_path_points(self, trip_id, board_stop_id, alight_stop_id):
+        
+        # connect to database
+        conn = psycopg2.connect(self.db_connect_string)
+        
+        # grab database cursor
+        cur = conn.cursor()
+        
+        # execute query to get board stop coordinates
+        cur.execute("select stop_lat, stop_lon from stops where stop_id='" + str(board_stop_id) + "'")
+        
+        # grab the board stop location
+        board_stop_loc = cur.fetchone()
+        
+        # send commit to the database
+        conn.commit()
+        
+        # execute query to get alight stop coordinates
+        cur.execute("select stop_lat, stop_lon from stops where stop_id='" + str(alight_stop_id) + "'")
+        
+        # grab the alight stop location
+        alight_stop_loc = cur.fetchone()
+        
+        # send commit to the database
+        conn.commit()
+        
+        # execute query to get trip shape id
+        cur.execute("select shape_id from trips where trip_id='" + str(trip_id) + "'")
+        
+        # grab the shape id
+        shape_id = cur.fetchone()[0]
+        
+        # check the shape id
+        if (shape_id.strip() == ''):
+            return [str(board_stop_loc[0]) + ',' + str(board_stop_loc[1]), str(alight_stop_loc[0]) + ',' + str(alight_stop_loc[1])]
+        
+        # send commit to the database
+        conn.commit()
         
         # execute query to get shape point sequence value for the board stop
         cur.execute("select shapes.shape_pt_sequence, ST_Distance(shapes.location, stops.location) as distance from shapes, stops where shapes.shape_id='" + shape_id + "' and stops.stop_id='" + str(board_stop_id) + "' order by distance asc limit 1")
@@ -41,7 +140,7 @@ class PostgresGIS_GTFSDB:
         board_shape_pt_sequence = cur.fetchone()[0]
         
         # send commit to the database
-        self.conn.commit()
+        conn.commit()
         
         # execute query to get shape point sequence value for the alight stop
         cur.execute("select shapes.shape_pt_sequence, ST_Distance(shapes.location, stops.location) as distance from shapes, stops where shapes.shape_id='" + shape_id + "' and stops.stop_id='" + str(alight_stop_id) + "' order by distance asc limit 1")
@@ -50,7 +149,7 @@ class PostgresGIS_GTFSDB:
         alight_shape_pt_sequence = cur.fetchone()[0]
         
         # send commit to the database
-        self.conn.commit()
+        conn.commit()
         
         # determine which point sequence value is greater
         if (board_shape_pt_sequence < alight_shape_pt_sequence):
@@ -66,7 +165,7 @@ class PostgresGIS_GTFSDB:
         path_points = cur.fetchall()
         
         # send commit to the database
-        self.conn.commit()
+        conn.commit()
         
         # iterate through points
         for i in range(len(path_points)):
@@ -74,6 +173,15 @@ class PostgresGIS_GTFSDB:
             point_lat = mod_point[mod_point.index(',')+1:]
             point_lon = mod_point[0:mod_point.index(',')]
             path_points[i] = point_lat + ',' + point_lon
+        
+        # close database connection
+        conn.close()
+        
+        # insert board stop location to front of path points list
+        path_points.insert(0, str(board_stop_loc[0]) + ',' + str(board_stop_loc[1]))
+        
+        # append alight stop location to end of path points list
+        path_points.append(str(alight_stop_loc[0]) + ',' + str(alight_stop_loc[1]))
         
         # return transit path points
         return path_points
--- pygs/graphserver/ext/osm/pgosmdb.py
+++ pygs/graphserver/ext/osm/pgosmdb.py
@@ -2,7 +2,7 @@
 # Class for handling interaction with OSMDB stored in PostgreSQL with PostGIS extensions.
 # Author: James P. Biagioni (jbiagi1@uic.edu)
 # Company: University of Illinois at Chicago
-# Last modified: 6/3/09
+# Last modified: 9/23/09
 #
 
 import psycopg2
@@ -14,16 +14,51 @@ class PostgresGIS_OSMDB:
     #
     def __init__(self, db_connect_string):
         
+        # store the connection string
+        self.db_connect_string = db_connect_string
+        
         # connect to database
-        self.conn = psycopg2.connect(db_connect_string)
+        #self.conn = psycopg2.connect(db_connect_string)
+    
+    #
+    # method for running a remote query against the PostgreSQL database
+    #
+    def execute(self, query, args=None):
+        
+        # connect to database
+        conn = psycopg2.connect(self.db_connect_string)
+        
+        # grab database cursor
+        cur = conn.cursor()
+        
+        # execute remote query with or without arguments
+        if (args is None):
+            cur.execute(query)
+        else:
+            cur.execute(query, args)
+        
+        # send commit to the database
+        conn.commit()
+        
+        # store the query result
+        query_result = cur.fetchall()
+        
+        # close database connection
+        conn.close()
+        
+        # return the query result
+        return query_result
     
     #
     # method for returning the closest osm vertex to a coordinate pair
     #
     def get_osm_vertex_from_coords(self, longitude, latitude):
         
+        # connect to database
+        conn = psycopg2.connect(self.db_connect_string)
+        
         # grab database cursor
-        cur = self.conn.cursor()
+        cur = conn.cursor()
         
         # place coordinates in POINT GIS object
         geom_point = "'POINT(" + str(longitude) + ' ' + str(latitude) + ")'"
@@ -38,18 +73,59 @@ class PostgresGIS_OSMDB:
         first_row = cur.fetchone()
         
         # send commit to the database
-        self.conn.commit()
-
+        conn.commit()
+        
+        # close database connection
+        conn.close()
+        
         # return osm vertex id
         return 'osm-' + first_row[0]
+    
+    #
+    # method for returning the coordinates (lat, lon) for an osm vertex
+    #
+    def get_coords_for_osm_vertex(self, vertex_id):
+        
+        # connect to database
+        conn = psycopg2.connect(self.db_connect_string)
+        
+        # grab database cursor
+        cur = conn.cursor()
+        
+        # strip 'osm-' prefix from vertex_id
+        vertex_id = vertex_id.replace('osm-','')
+        
+        # generate query to grab coordinates for vertex
+        vertex_query = "select ST_AsText(location) from nodes where id='" + vertex_id + "'"
+        
+        # execute the query
+        cur.execute(vertex_query)
+        
+        # fetch the first row from the results
+        first_row = cur.fetchone()
+        
+        # send commit to the database
+        conn.commit()
+        
+        # grab raw coordinates
+        vertex_coords = first_row[0].replace('POINT(','').replace(')','')
+        
+        # close database connection
+        conn.close()
+        
+        # return coordinates (lat, lon)
+        return (float(vertex_coords[vertex_coords.index(' ')+1:]), float(vertex_coords[0:vertex_coords.index(' ')]))
     
     #
     # method for returning the street name for a graph edge
     #
     def get_street_name_from_edge(self, edge_name):
         
+        # connect to database
+        conn = psycopg2.connect(self.db_connect_string)
+        
         # grab database cursor
-        cur = self.conn.cursor()
+        cur = conn.cursor()
         
         # generate query to grab tags for way matching the edge name
         tags_query = "select tags from ways where id='" + edge_name + "'"
@@ -61,7 +137,7 @@ class PostgresGIS_OSMDB:
         first_row = cur.fetchone()
         
         # send commit to the database
-        self.conn.commit()
+        conn.commit()
         
         # create dictionary from returned tags
         street_dict = eval(first_row[0])
@@ -77,6 +153,9 @@ class PostgresGIS_OSMDB:
             # store an 'Unknown' street name
             street_name = "Unknown"
         
+        # close database connection
+        conn.close()
+        
         # return the street name
         return street_name
     
@@ -85,8 +164,11 @@ class PostgresGIS_OSMDB:
     #
     def get_street_name_and_path_geometry_from_edge(self, edge_name):
         
+        # connect to database
+        conn = psycopg2.connect(self.db_connect_string)
+        
         # grab database cursor
-        cur = self.conn.cursor()
+        cur = conn.cursor()
         
         # generate query to grab tags and path geometry for way matching the edge name
         tags_path_query = "select tags, ST_AsText(path) from ways where id='" + edge_name + "'"
@@ -98,7 +180,7 @@ class PostgresGIS_OSMDB:
         first_row = cur.fetchone()
         
         # send commit to the database
-        self.conn.commit()
+        conn.commit()
         
         # create dictionary from returned tags
         street_dict = eval(first_row[0])
@@ -116,6 +198,9 @@ class PostgresGIS_OSMDB:
         
         # store the path geometry
         path_geometry = str(first_row[1])
+        
+        # close database connection
+        conn.close()
         
         # return the street name and path geometry
         return (street_name, path_geometry)
