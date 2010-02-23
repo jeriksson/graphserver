7578457a80f7da7eb72d78e543efb1dfd3eb67e6
James Biagioni <james@transitgenie.com>
2010-02-22 18:19:54 -0600

    added version argument to path_xml parameter list

--- apps/routeserver/routeserver_xml.py
+++ apps/routeserver/routeserver_xml.py
@@ -84,7 +84,7 @@ class RouteServer(Servable):
         
     transit_path.mime = "text/xml"
     
-    def path_xml(self, origlon, origlat, destlon, destlat, dep_time=0, arr_time=0, timezone="", transfer_penalty=100, walking_speed=1.0, walking_reluctance=1.0, max_walk=10000, walking_overage=0.1, seqno=0, street_mode="walk", less_walking="False", udid=""):
+    def path_xml(self, origlon, origlat, destlon, destlat, dep_time=0, arr_time=0, timezone="", transfer_penalty=100, walking_speed=1.0, walking_reluctance=1.0, max_walk=10000, walking_overage=0.1, seqno=0, street_mode="walk", less_walking="False", udid="", version="2.0"):
         
         # acquire lock
         self.path_xml_lock.acquire()
